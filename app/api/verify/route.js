6f409ff221f82ed260f6e5995598c5646621668b
EuodiaWholeFoods <euodiawholefoods.dish@gmail.com>
2024-12-12 01:03:14 +0100

    changhing app folder

--- app/api/verify/route.js
+++ app/api/verify/route.js
@@ -2,8 +2,6 @@ import { verifyPaystackPayment } from "@/utils/lib/verify";
 import { updateTransactionStatus } from "@/utils/sanity/client";
 
 export const dynamic = "force-dynamic";
-
-
 export const GET = async (req) => {
   try {
     const { searchParams } = new URL(req.url);
